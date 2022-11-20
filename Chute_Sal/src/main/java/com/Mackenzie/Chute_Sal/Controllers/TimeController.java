753380d1c4cabf2faadd5aef213882ba47bf05c3
Julio Mota <86507734+Juliomtta@users.noreply.github.com>
2022-11-20 18:43:51 -0300

    Update TimeController.java

--- Chute_Sal/src/main/java/com/Mackenzie/Chute_Sal/Controllers/TimeController.java
+++ Chute_Sal/src/main/java/com/Mackenzie/Chute_Sal/Controllers/TimeController.java
@@ -2,12 +2,15 @@ package com.Mackenzie.Chute_Sal.Controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,33 +21,48 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Mackenzie.Chute_Sal.Models.Time;
 import com.Mackenzie.Chute_Sal.Repositories.TimeRepository;
 
-@Controller
+@RestController
 @RequestMapping("/time")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TimeController {
 
     @Autowired
     TimeRepository timeRepository;
 
-    @GetMapping("/manager/")
-    public String time(){
-        return "Manager/criarTime";
+    @GetMapping("/")
+    public List<Time> time(){
+        return timeRepository.findAll();
     }   
 
-    @PostMapping("/manager/")
-    public String time(Time time){
-        timeRepository.save(time);
-        return "/Manager/criarTime";
+    @GetMapping("/{id}")
+    public Optional<Time> campeonato(@PathVariable("id") long id){
+
+        return timeRepository.findById(id);
     }
 
-    @DeleteMapping("/")
-    public void deleteTime(@RequestBody Long id){
-        timeRepository.deleteById(id);
-    }
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody Time time){
 
-    @PutMapping("/")
-    public ResponseEntity atualizaTime(@RequestBody Time time){
         timeRepository.save(time);
         return ResponseEntity.ok().body(time);
+
     }
+    @DeleteMapping("/{id}")
+    public void deleteTime(@PathVariable("id") Long Id){
+        timeRepository.deleteById(Id);
+           
+    }
+
+    
+    @PutMapping("/")
+    public ResponseEntity atualizaUnidade(@RequestBody Time time){
+        Time timeAtu = new Time();
+        timeAtu.setId(time.getId());
+        timeAtu.setNome(time.getNome());
+        timeAtu.setCampeonato(time.getCampeonato());
+        timeRepository.save(timeAtu);
+        return ResponseEntity.ok().body(timeAtu);
+    }
+
     
 }
