0c5fe564572068a786e1ec146ccef9a108e1610a
Julio Mota <86507734+Juliomtta@users.noreply.github.com>
2022-11-21 12:47:31 -0300

    Update JogoController.java

--- Chute_Sal/src/main/java/com/Mackenzie/Chute_Sal/Controllers/JogoController.java
+++ Chute_Sal/src/main/java/com/Mackenzie/Chute_Sal/Controllers/JogoController.java
@@ -2,11 +2,15 @@ package com.Mackenzie.Chute_Sal.Controllers;
 
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,36 +20,53 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Mackenzie.Chute_Sal.Models.Jogo;
 import com.Mackenzie.Chute_Sal.Repositories.JogoRepository;
 
-@Controller
+@RestController
 @RequestMapping("/jogo")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JogoController {
     
     @Autowired
     JogoRepository jogoRepository;
 
-    @GetMapping("/manager/")
-    public String jogo(){
-        return "Manager/criarJogo";
+    @GetMapping("/")
+    public List<Jogo> jogo(){
+       return jogoRepository.findAll();
     }
 
-    @PostMapping("/manager/")
-    public String  jogo(Jogo jogo){
+    @GetMapping("/{id}")
+    public Optional<Jogo> jogo(@PathVariable("id") long id){
+
+        return jogoRepository.findById(id);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody Jogo jogo){
+
         jogoRepository.save(jogo);
-        return "Manager/criarJogo";
-    }
-
-    @DeleteMapping("/")
-    public ResponseEntity deletaJogo(@RequestBody Jogo jogo){
-        jogoRepository.deleteById(jogo.getId());
         return ResponseEntity.ok().body(jogo);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long Id){
+        jogoRepository.deleteById(Id);
+           
     }
     
-        @PutMapping("/")
-        public ResponseEntity atualizaManager(@RequestBody Jogo jogo){
-            jogoRepository.save(jogo);
-    
-            return ResponseEntity.ok().body(jogo);
-    
-
-        }
+    @PutMapping("/")
+    public ResponseEntity atualizaUnidade(@RequestBody Jogo jogo){
+        Jogo jogoAtu = new Jogo();
+        jogoAtu.setId(jogo.getId());
+        jogoAtu.setTime1(jogo.getTime1());
+        jogoAtu.setTime2(jogo.getTime2());
+        jogoAtu.setPontTime1(jogo.getPontTime1());
+        jogoAtu.setPontTime2(jogo.getPontTime2());
+        jogoAtu.setData(jogo.getData());
+        jogoAtu.setHora(jogo.getHora());
+        jogoAtu.setStatus(jogo.getStatus());
+        jogoAtu.setVencedor(jogo.getVencedor());
+        jogoRepository.save(jogoAtu);
+        return ResponseEntity.ok().body(jogoAtu);
+    }
 }
